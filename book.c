37d62caa19b3a5e37cdb2ffb0ebd9a96c07285e3
diaoyiduoxue <127192116+diaoyiduoxue@users.noreply.github.com>
2023-07-10 21:21:46 +0800

    Update book.c

--- book.c
+++ book.c
@@ -1,1 +1,18 @@
+#include "book.h"
+#include"config.h"
+Book* createmptybook()
+{
+	Book* book = calloc(1, sizeof(Book));
+	if (!book)
+	{
+		printf("creat emptyuser faild\n");
+		return NULL;
+	}
+	return book;
+    
+}
 
+void printbook(Book* book)
+{
+	printf("\033[38;2;180;255;245m%-8llu %-5llu %-10s %-10s %-10s %-5d %-6d \033[0m\n",book->recordNO,book->bookNO, book->name, book->author, book->press, book->bookcnt, book->borrowcnt);
+}
