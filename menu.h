5836f60455a256437cab5d816a2b8875838834e1
diaoyiduoxue <127192116+diaoyiduoxue@users.noreply.github.com>
2023-07-10 21:30:06 +0800

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,1 +1,17 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+int welcomemenu();
 
+int mainmenu();
+
+int usermenu();
+
+int readermenu();
+
+int bookmenu();
+
+int booksearchmenu();
+
+int bookcirculatemenu();
+
+#endif
