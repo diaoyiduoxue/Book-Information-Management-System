ae63b6365ae3e005eabf84df9a443edef7671e33
diaoyiduoxue <127192116+diaoyiduoxue@users.noreply.github.com>
2023-07-10 21:35:48 +0800

    Update user.c

--- user.c
+++ user.c
@@ -1,1 +1,33 @@
+#include "user.h"
+#include"config.h"
 
+User* createmptyuser()
+{
+	User* user = calloc(1, sizeof(User));
+	if (!user)
+	{
+		printf("creat emptyuser faild\n");
+		return NULL;
+	}
+	return user;
+}
+
+void printuser(User* user)
+{
+	printf("\033[03;01m\033[38;2;180;255;245m%-10llu %-16s %10d\n", user->number, user->passward, user->type);
+}
+
+bool isSysad(User* user)
+{
+	return user->type==ut_sysadminuser;
+}
+
+bool isBookad(User* user)
+{
+	return user->type==ut_bookadminuser;
+}
+
+bool isComuser(User* user)
+{
+	return user->type==ut_commentuser;
+}
