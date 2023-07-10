0ebc0eaeedb8b07cb3231e94d920b1a5fc089b73
diaoyiduoxue <127192116+diaoyiduoxue@users.noreply.github.com>
2023-07-10 21:22:48 +0800

    Update bookcircle.c

--- bookcircle.c
+++ bookcircle.c
@@ -1,1 +1,17 @@
+#include "bookcircle.h"
+#include"config.h"
+Circulate* createmptycir()
+{
+	Circulate* cir = calloc(1, sizeof(Circulate));
+	if (!cir)
+	{
+		printf("creat emptyuser faild\n");
+		return NULL;
+	}
+    return cir;
+}
 
+void cirprint(Circulate* cir)
+{
+	printf("\033[03;01m\033[38;2;180;255;245m%-8llu %-8d %-10s %-10s\t %s", cir->readerNO, cir->bookNO, cir->borrowdate, cir->returndate, cir->remark);
+}
