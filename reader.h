116394415d7aba7101bac25f1c8b6428edde1cf6
diaoyiduoxue <127192116+diaoyiduoxue@users.noreply.github.com>
2023-07-10 21:30:53 +0800

    Update reader.h

--- reader.h
+++ reader.h
@@ -1,1 +1,17 @@
+#ifndef _READER_H_
+#define _READER_H_
 
+typedef struct Reader
+{
+	unsigned long long readerID;  //读者号
+	char readername[20];//读者姓名
+	char readerworkplace[30];//工作单位
+	char readertel[15];//联系方式
+	int canborrowcnt;//可借书数
+	int yetborrowcnt;//已经借书数
+}Reader;
+Reader* createmptyreader();
+void printreader(Reader* reader);
+
+
+#endif
