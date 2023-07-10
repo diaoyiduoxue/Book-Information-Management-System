1240ce666c5fb4ee392d54a6ce6634a156c17500
diaoyiduoxue <127192116+diaoyiduoxue@users.noreply.github.com>
2023-07-10 21:23:11 +0800

    Update bookcircle.h

--- bookcircle.h
+++ bookcircle.h
@@ -1,1 +1,14 @@
+#ifndef _BOOKCIRCLE_H_
+#define _BOOKCIRCLE_H_
+typedef struct Circulate
+{
+	unsigned long long readerNO;//读者ID
+	int bookNO;//书籍NO
+	char borrowdate[11];
+	char returndate[11];
+	char remark[16];
+}Circulate;
+Circulate* createmptycir();
+void cirprint(Circulate* cir);
 
+#endif
