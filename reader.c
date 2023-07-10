3e0fb3c01d51107443424caffba6ea8da6053fd6
diaoyiduoxue <127192116+diaoyiduoxue@users.noreply.github.com>
2023-07-10 21:30:30 +0800

    Update reader.c

--- reader.c
+++ reader.c
@@ -1,1 +1,19 @@
+#include "reader.h"
+#include"config.h"
+Reader* createmptyreader()
+{
+	Reader* reader = calloc(1, sizeof(Reader));
+	if (!reader)
+	{
+		printf("creat emptyuser faild\n");
+		return NULL;
+	}
+	return reader;
+    
+}
+
+void printreader(Reader* reader)
+{
+	printf("\033[03;01m\033[38;2;180;255;245m%-10llu %-10s %-20s %-15s \t%d\t %d\n", reader->readerID, reader->readername, reader->readerworkplace, reader->readertel, reader->canborrowcnt, reader->yetborrowcnt);
+}
 
